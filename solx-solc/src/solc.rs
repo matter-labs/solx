4f5fe0f2729570648f5ce72d4fa0ef94d0858200
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-02-19 22:06:01 +0800

    rebuild solx if any dependencies are changed

--- solx-solc/src/solc.rs
+++ solx-solc/src/solc.rs
@@ -2,6 +2,7 @@
 //! The Solidity compiler.
 //!
 
+use std::borrow::Cow;
 use std::ffi::CStr;
 use std::ffi::CString;
 use std::path::PathBuf;
@@ -25,12 +26,23 @@ pub struct Compiler {
 
 #[link(name = "solc", kind = "static")]
 extern "C" {
-    // Match the function signatures exactly.
-    // If your return type is `const char*`, you might wrap it in Rust with `*const c_char`.
-    // If you wrote a custom bridging function, rename to match that bridging function.
+    ///
+    /// Pass standard JSON input to the Solidity compiler.
+    ///
+    /// Passes `--base-path`, `--include-paths`, and `--allow-paths` just like it is done with the CLI.
+    ///
+    fn solidity_compile_default_callback(
+        input: *const ::libc::c_char,
+        base_path: *const ::libc::c_char,
+        include_paths_size: u64,
+        include_paths: *const *const ::libc::c_char,
+        allow_paths_size: u64,
+        allow_paths: *const *const ::libc::c_char,
+    ) -> *const std::os::raw::c_char;
 
-    fn solidity_compile(input: *const std::os::raw::c_char) -> *const std::os::raw::c_char;
-
+    ///
+    /// Get the Solidity compiler version.
+    ///
     fn solidity_version() -> *const std::os::raw::c_char;
 }
 
@@ -44,23 +56,78 @@ impl Default for Compiler {
 
 impl Compiler {
     /// The last ZKsync revision of `solc`.
-    pub const LAST_ZKSYNC_REVISION: semver::Version = semver::Version::new(0, 1, 0);
+    pub const LAST_ZKSYNC_REVISION: semver::Version = semver::Version::new(1, 0, 1);
 
     ///
     /// The Solidity `--standard-json` mirror.
     ///
     pub fn standard_json(
         &self,
-        input: &mut StandardJsonInput,
+        input_json: &mut StandardJsonInput,
         messages: &mut Vec<StandardJsonOutputError>,
         base_path: Option<String>,
         include_paths: Vec<String>,
         allow_paths: Option<String>,
     ) -> anyhow::Result<StandardJsonOutput> {
-        let input_string = serde_json::to_string(input).expect("Always valid");
-        let input_ffi = CString::new(input_string).expect("Always valid");
+        let input_string = serde_json::to_string(input_json).expect("Always valid");
+        let input_string = Self::to_null_terminated_owned(input_string.as_str());
+        let input_c_string = Self::to_c_str(input_string.as_str());
+
+        let base_path = base_path.as_deref().map(Self::to_null_terminated_owned);
+        let base_path = base_path
+            .as_ref()
+            .map(|base_path| Self::to_c_str(base_path.as_str()));
+        let base_path = match base_path {
+            Some(base_path) => base_path.as_ptr(),
+            None => std::ptr::null(),
+        };
+
+        let include_paths: Vec<String> = include_paths
+            .iter()
+            .map(|path| Self::to_null_terminated_owned(path))
+            .collect();
+        let include_paths: Vec<Cow<CStr>> = include_paths
+            .iter()
+            .map(|path| Self::to_c_str(path.as_str()))
+            .collect();
+        let include_paths: Vec<*const ::libc::c_char> =
+            include_paths.iter().map(|path| path.as_ptr()).collect();
+        let include_paths_ptr = if include_paths.is_empty() {
+            std::ptr::null()
+        } else {
+            include_paths.as_ptr()
+        };
+
+        let allow_paths = allow_paths
+            .as_deref()
+            .map(|allow_paths| {
+                allow_paths
+                    .split(',')
+                    .map(Self::to_null_terminated_owned)
+                    .collect::<Vec<String>>()
+            })
+            .unwrap_or_default();
+        let allow_paths: Vec<Cow<CStr>> = allow_paths
+            .iter()
+            .map(|path| Self::to_c_str(path.as_str()))
+            .collect();
+        let allow_paths: Vec<*const ::libc::c_char> =
+            allow_paths.iter().map(|path| path.as_ptr()).collect();
+        let allow_paths_ptr = if allow_paths.is_empty() {
+            std::ptr::null()
+        } else {
+            allow_paths.as_ptr()
+        };
+
         let output_ffi = unsafe {
-            let output_pointer = solidity_compile(input_ffi.as_ptr());
+            let output_pointer = solidity_compile_default_callback(
+                input_c_string.as_ptr(),
+                base_path,
+                include_paths.len() as u64,
+                include_paths_ptr,
+                allow_paths.len() as u64,
+                allow_paths_ptr,
+            );
             CStr::from_ptr(output_pointer)
                 .to_string_lossy()
                 .into_owned()
@@ -83,8 +150,8 @@ impl Compiler {
             });
         solc_output.errors.append(messages);
 
-        input.resolve_sources();
-        solc_output.preprocess_ast(&input.sources, &self.version)?;
+        input_json.resolve_sources();
+        solc_output.preprocess_ast(&input_json.sources, &self.version)?;
         solc_output.remove_evm_artifacts();
 
         Ok(solc_output)
@@ -140,5 +207,25 @@ impl Compiler {
             .expect("Always valid");
 
         Version::new(output, default, Self::LAST_ZKSYNC_REVISION)
+    }
+
+    fn to_c_str(mut s: &str) -> Cow<CStr> {
+        if s.is_empty() {
+            s = "\0";
+        }
+
+        if !s.chars().rev().any(|ch| ch == '\0') {
+            return Cow::from(CString::new(s).expect("unreachable since null bytes are checked"));
+        }
+
+        unsafe { Cow::from(CStr::from_ptr(s.as_ptr() as *const _)) }
+    }
+
+    fn to_null_terminated_owned(s: &str) -> String {
+        if let Some(p) = s.rfind('\0') {
+            s[..=p].to_string()
+        } else {
+            format!("{s}\0")
+        }
     }
 }
