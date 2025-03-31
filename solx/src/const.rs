66464008733963345b4c76b3aee65d0548c202ef
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-03-31 22:56:12 +0800

    add the executable name constant

--- solx/src/const.rs
+++ solx/src/const.rs
@@ -2,6 +2,9 @@
 //! Solidity compiler constants.
 //!
 
+/// The default executable name.
+pub static DEFAULT_EXECUTABLE_NAME: &str = env!("CARGO_PKG_NAME");
+
 /// The worker thread stack size.
 pub const WORKER_THREAD_STACK_SIZE: usize = 64 * 1024 * 1024;
 
