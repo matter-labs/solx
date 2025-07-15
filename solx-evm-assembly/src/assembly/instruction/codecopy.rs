818f0c780973e72f348e8fa88106d9e549cb39b2
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-07-15 19:02:38 +0400

    feat: support CODECOPYing constant byte arrays

--- solx-evm-assembly/src/assembly/instruction/codecopy.rs
+++ solx-evm-assembly/src/assembly/instruction/codecopy.rs
@@ -33,32 +33,24 @@ pub fn static_data<'ctx>(
     destination: inkwell::values::IntValue<'ctx>,
     source: &str,
 ) -> anyhow::Result<()> {
-    let mut offset = 0;
-    for (index, chunk) in source
-        .chars()
-        .collect::<Vec<char>>()
-        .chunks(era_compiler_common::BYTE_LENGTH_FIELD * 2)
-        .enumerate()
-    {
-        let mut value_string = chunk.iter().collect::<String>();
-        value_string.push_str(
-            "0".repeat((era_compiler_common::BYTE_LENGTH_FIELD * 2) - chunk.len())
-                .as_str(),
-        );
+    let pointer = era_compiler_llvm_context::Pointer::new_with_offset(
+        context,
+        era_compiler_llvm_context::EVMAddressSpace::Heap,
+        context.field_type(),
+        destination,
+        "codecopy_bytes_destination_pointer",
+    )?;
 
-        let datacopy_destination = context.builder().build_int_add(
-            destination,
-            context.field_const(offset as u64),
-            format!("datacopy_destination_index_{index}").as_str(),
-        )?;
-        let datacopy_value = context.field_const_str_hex(value_string.as_str());
-        era_compiler_llvm_context::evm_memory::store(
-            context,
-            datacopy_destination,
-            datacopy_value,
-        )?;
-        offset += chunk.len() / 2;
-    }
-
+    context.build_call_metadata(
+        context.intrinsics().codecopybytes,
+        &[
+            pointer.as_basic_value_enum().into(),
+            context
+                .llvm()
+                .metadata_node(&[context.llvm().metadata_string(source).into()])
+                .into(),
+        ],
+        "codecopy_bytes",
+    )?;
     Ok(())
 }
