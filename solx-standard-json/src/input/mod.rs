527755f9b63c923814b70570f1d77faccde2f6ef
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-08-27 19:08:53 +0400

    add docs and tests

--- solx-standard-json/src/input/mod.rs
+++ solx-standard-json/src/input/mod.rs
@@ -49,7 +49,6 @@ impl Input {
             None => std::io::read_to_string(std::io::stdin())
                 .map_err(|error| anyhow::anyhow!("Standard JSON reading from stdin: {error}")),
         }?;
-        // TODO: write docs
         if let Ok(output_path) = std::env::var("SOLX_STANDARD_JSON_DEBUG") {
             std::fs::write(output_path.as_str(), input_json.as_str()).map_err(|error| {
                 anyhow::anyhow!("Standard JSON input debug file `{output_path}` writing: {error}")
